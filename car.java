6375973a668b732c8626bb9206bfd1c66f53aaae
manjula2711 <130962788+manjula2711@users.noreply.github.com>
2023-07-10 00:01:14 +0530

    Update car.java

--- car.java
+++ car.java
@@ -7,7 +7,7 @@ public class car extends cardetails{
 		System.out.print("Select car model: ");
 		cd.setModel(sc.nextLine());
 		System.out.print("Do you need insurence: ");
-        cd.setInsurence(sc.nextLine());
+                cd.setInsurence(sc.nextLine());
 		System.out.print("Do you need Additional Accessories: ");
 		cd.setAccessories(sc.nextLine());
 		System.out.print("Dealer discount: ");
@@ -17,53 +17,53 @@ public class car extends cardetails{
 		case "Polo Tredline":
 			cd.setCost(870000);
 			long cost =cd.getCost();
-//			System.out.println("showroom price of selected car: "+cost);
+			cd.totalcost(cmodel,cost);
 			break;
 			
 		
 		case "Polo Highline":
 			cd.setCost(1009000);
 			cost =cd.getCost();
-//			System.out.println("showroom price of selected car: "+cost);			
+			cd.totalcost(cmodel,cost);
 			break;
 			
 		
 		case "Vitus Tredline":
 			cd.setCost(1105000);
 			cost =cd.getCost();
-//			System.out.println("showroom price of selected car: "+cost);
+			cd.totalcost(cmodel,cost);
 			break;
 		
 		case "Vitus Highline":
 			cd.setCost(1308000);
 			cost =cd.getCost();
-//			System.out.println("showroom price of selected car: "+cost);
+			cd.totalcost(cmodel,cost);
 			break;
 		
 		case "Taigun Tredline":
 			cd.setCost(1489000);
 			cost =cd.getCost();
-//			System.out.println("showroom price of selected car: "+cost);
+			cd.totalcost(cmodel,cost);
 			break;
 		
 		case "Taigun Highline":
 			cd.setCost(1542000);
 			cost =cd.getCost();
-//			System.out.println("showroom price of selected car: "+cost);
+			cd.totalcost(cmodel,cost);
+
 			break;
 		
 		case "Taigun Topline":
 			cd.setCost(1771000);
 			cost =cd.getCost();
-//			System.out.println("showroom price of selected car: "+cost);
+			cd.totalcost(cmodel,cost);
 			break;
 		default:
 		       System.out.println();		
 		       break;
 		
 		}
-		long cost = cd.getCost();
-		cd.totalcost(cmodel,cost);
+		
         
 	}
 	
